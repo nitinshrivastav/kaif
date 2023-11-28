5cf3cdbc352cd5839f5c034cc00888667f7a7215
harshita <harshita@gail.com>
2023-11-28 20:40:16 +0530

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 def input():
-    pass
-def sum():
-    pass
-def minus():
-    pass
+    a=int(input("enter the number"))
+    b=int(input("enter the value"))
+    num=int(input("enter the choice"))
+    return(a,b,num)     
+             
+def sum(a,b):
+    return(a+b)
+def minus(x,y):
+    return(x-y)
 def result():
     pass
